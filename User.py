5c0f537b23c02f8c0404eaf44718a40a21b59975
Codey Funston <cfunstondev@gmail.com>
2023-05-05 22:48:00 +1000

    deleted test.py and added User class

---
+++ User.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, email: str, balance: float) -> None:             
+        self.email = email
+        self.balance = balance
+
+    def Print():
+        print("test")
